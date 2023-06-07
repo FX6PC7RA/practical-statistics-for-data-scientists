baaaf38ebc38e169638f9da411d2303b45b8493e
Денис Галиньш <denisgalinsh@MacBook-Pro-Deniss.local>
2023-06-07 11:37:41 +0300

    ok

--- R/code/Chapter 2 - Data and sampling distributions.R
+++ R/code/Chapter 2 - Data and sampling distributions.R
@@ -9,7 +9,7 @@ library(ggplot2)
 
 # Define paths to data sets. If you don't keep your data in the same directory as the code, adapt the path names.
 
-PSDS_PATH <- file.path(dirname(dirname(getwd())))
+PSDS_PATH <- getwd()
 
 loans_income <- read.csv(file.path(PSDS_PATH, 'data', 'loans_income.csv'))
 loans_income <- loans_income[, 1]   # convert data frame to vector
@@ -23,34 +23,34 @@ gauss <- dnorm(x)
     par(mar=c(3, 3, 0, 0)+.1)
     plot(x, gauss, type='l', col='blue', xlab='', ylab='', axes=FALSE)
     polygon(x, gauss, col='blue')
-dev.off()
+    #dev.off()
 
     norm_samp <- rnorm(100)
     par(mar=c(3, 3, 0, 0)+.1)
     hist(norm_samp, axes=FALSE, col='red', main='')
-dev.off()
+    #dev.off()
 
 ## Sampling Distribution of a Statistic
 
 # take a simple random sample
-samp_data <- data.frame(income=sample(loans_income, 1000), 
+samp_data <- data.frame(income=sample(loans_income, 1000),
                         type='data_dist')
 
 # take a sample of means of 5 values
 samp_mean_05 <- data.frame(
-  income = tapply(sample(loans_income, 1000*5), 
+  income = tapply(sample(loans_income, 1000*5),
                   rep(1:1000, rep(5, 1000)), FUN=mean),
   type = 'mean_of_5')
 
 # take a sample of means of 20 values
 samp_mean_20 <- data.frame(
-  income = tapply(sample(loans_income, 1000*20), 
+  income = tapply(sample(loans_income, 1000*20),
                   rep(1:1000, rep(20, 1000)), FUN=mean),
   type = 'mean_of_20')
 
 # bind the data.frames and convert type to a factor
 income <- rbind(samp_data, samp_mean_05, samp_mean_20)
-income$type <- factor(income$type, 
+income$type <- factor(income$type,
                      levels=c('data_dist', 'mean_of_5', 'mean_of_20'),
                      labels=c('Data', 'Mean of 5', 'Mean of 20'))
 
@@ -61,8 +61,8 @@ ggplot(income, aes(x=income)) +
 ## The Bootstrap
 # As the calculation uses random samples, results will vary between runs
 
-stat_fun <- function(x, idx) median(x[idx])
-boot_obj <- boot(loans_income, R=1000, statistic=stat_fun)
+stat_fun <- function(x, idx) mean(x[idx])
+boot_obj <- boot(loans_income, R=10000, statistic=stat_fun)
 
 boot_obj
 
@@ -74,6 +74,9 @@ set.seed(7)
 sample20 <- sample(loans_income, 20)
 sampleMean <- mean(sample20)
 
+sample1000 <- sample(loans_income, 1000)
+sampleMean1000 <- mean(sample1000)
+# Sample of size 20
 stat_fun <- function(x, idx) mean(x[idx])
 boot_obj <- boot(sample20, R=500, statistic=stat_fun)
 boot_ci <- boot.ci(boot_obj, conf=0.9, type='basic')
@@ -90,8 +93,28 @@ ggplot(X, aes(x=mean)) +
     geom_path(aes(x=ci90[2], y=y), data=ci, size=2) +
     geom_text(aes(x=sampleMean, y=20, label='Sample mean'), size=6) +
     geom_text(aes(x=sampleMean, y=8, label='90% interval'), size=6) +
-    theme_bw() + 
+    theme_bw() +
     labs(x='', y='Counts')
+
+# Sample of size 1000
+stat_fun <- function(x, idx) mean(x[idx])
+boot_obj <- boot(sample1000, R=500, statistic=stat_fun)
+boot_ci <- boot.ci(boot_obj, conf=0.9, type='basic')
+X <- data.frame(mean=boot_obj$t)
+ci90 <- boot_ci$basic[4:5]
+ci <- data.frame(cix=ci90, ciy=c(9, 11))
+ #ci <- boot_ci$basic[4:5]
+ci
+ggplot(X, aes(x=mean)) +
+  geom_histogram(bins=40, fill='#AAAAAA') +
+  geom_vline(xintercept=sampleMean1000, linetype=2) +
+  geom_path(aes(x=cix, y=10), data=ci, size=2) +
+  geom_path(aes(x=ci90[1], y=ciy), data=ci, size=2) +
+  geom_path(aes(x=ci90[2], y=ciy), data=ci, size=2) +
+  geom_text(aes(x=sampleMean1000, y=20, label='Sample mean'), size=6) +
+  geom_text(aes(x=sampleMean1000, y=8, label='90% interval'), size=6) +
+  theme_bw() +
+  labs(x='', y='Counts')
 
 ## Normal Distribution
 ### Standard Normal and QQ-Plots
